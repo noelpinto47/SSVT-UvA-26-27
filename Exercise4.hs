956d35d2b505b6507e5e615ba94e6bb5f4d3c56a
Angus Thom <angus.thom@student.uva.nl>
2026-09-22 18:10:44 +0200

    Update Exercise4.hs comments

--- Exercise4.hs
+++ Exercise4.hs
@@ -1,12 +1,14 @@
 module Exercise4 where
 
+-- CHECKS IF TWO LISTS ARE PERMUTATIONS OF EACH OTHER
+
 -- EXERCISE 4
--- TIME SPENT: 1.5 hours
+-- TIME SPENT: 3.5 hours
 
 import Data.List (sort, nub)
 import Test.QuickCheck
 
--- two lists are permutations of each other if they contain the same elements, possibly in a different order
+-- two lists are permutations of each other if they contain the same elements, even in a different order
 isPermutation :: (Ord a) => [a] -> [a] -> Bool
 isPermutation list1 list2 = sort list1 == sort list2
 
@@ -15,25 +17,32 @@ isPermutation list1 list2 = sort list1 == sort list2
 genNoDups :: Gen [Int]
 genNoDups = nub <$> arbitrary
 
+
+
+
+
 -- property 1: a list is always a permutation of itself
 prop_permReflexive :: Property
 prop_permReflexive = forAll genNoDups $ \list1 -> isPermutation list1 list1
 
+
 -- property 2: permutation is symmetric
 -- if list1 is a permutation of list2, then list2 is a permutation of list1
 prop_permSymmetric :: Property
-prop_permSymmetric = forAll genNoDups $ \list1 ->
-    forAll (shuffle list1) $ \list2 ->
-        isPermutation list1 list2 == isPermutation list2 list1
+prop_permSymmetric = forAll genNoDups $ \list1 -> forAll (shuffle list1) $ \list2 -> isPermutation list1 list2 == isPermutation list2 list1
+
 
 -- property 3: if two lists are permutations of each other, they must have the same length
 prop_permSameLength :: Property
 prop_permSameLength = forAll genNoDups $ \list1 -> forAll (shuffle list1) $ \list2 -> isPermutation list1 list2 ==> length list1 == length list2
 
--- property 4: known permutation test
--- if we shuffle a list, it should be a permutation of that original list
+
+-- property 4: if shuffle a list, it should be a permutation of that original list
 prop_permKnown :: Property
 prop_permKnown = forAll genNoDups $ \list1 -> forAll (shuffle list1) $ \list2 -> isPermutation list1 list2 == (sort list1 == sort list2)
+
+
+
 
 -- default value for lists
 orDefault :: [a] -> [a] -> [a]
